e2b816df4f88c5448fa30af4d4a453305bda290c
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-04-18 11:17:49 +0200

    arggh

--- TarmicasYoutubeDownloader.py
+++ TarmicasYoutubeDownloader.py
@@ -1,33 +1,49 @@
-import base64
 import streamlit as st
 import pytube
-import uuid
+import base64
 import os
+import uuid
 
-st.set_page_config(page_title="Tarmic's YouTube Downloader", page_icon=":arrow_down:")
+# Set the download folder path
+DOWNLOAD_PATH = os.path.expanduser("~") + "/Downloads/"
 
-st.title("Tarmic's YouTube Downloader")
 
-url = st.text_input("Enter the YouTube video URL below", "")
-if url:
-    try:
-        video = pytube.YouTube(url)
+# Generate unique filename using uuid
+def generate_filename(video):
+    return f"{video.title}-{str(uuid.uuid4())[:8]}.mp4"
 
-        st.write("Title:", video.title)
-        st.write("Length:", video.length, "seconds")
 
-        with st.spinner("Downloading..."):
-            stream = video.streams.get_highest_resolution()
-            file_id = str(uuid.uuid4())
-            stream.download(filename=file_id)
-        st.success("Download Successful!")
+# Define a function to download the video
+def download_video(url):
+    # Get the video details
+    st.write("Getting video details...")
+    video = pytube.YouTube(url)
 
-        file_path = os.path.join(os.getcwd(), f"{file_id}.mp4")
-        st.write("Download link:")
-        st.markdown(
-            f'<a href="data:file/mp4;base64,{base64.b64encode(open(file_path, "rb").read()).decode()}" download="{video.title}.mp4">Generate link</a>',
-            unsafe_allow_html=True,
-        )
+    # Get the highest resolution stream
+    st.write("Getting highest resolution stream...")
+    stream = video.streams.get_highest_resolution()
 
-    except pytube.exceptions.PytubeError as e:
-        st.error(str(e))
+    # Download the video
+    st.write("Downloading video...")
+    file_path = DOWNLOAD_PATH + generate_filename(video)
+    stream.download(output_path=DOWNLOAD_PATH, filename=generate_filename(video))
+
+    # Generate the download link
+    st.markdown(
+        f'<a href="data:file/mp4;base64,{base64.b64encode(open(file_path, "rb").read()).decode()}" download="{generate_filename(video)}">Generate link to download {video.title}</a>',
+        unsafe_allow_html=True,
+    )
+
+
+# Set the app title
+st.set_page_config(page_title="Tarmicas Youtube Downloader")
+
+# Display the app title
+st.title("Tarmicas Youtube Downloader")
+
+# Get the video URL from the user
+url = st.text_input("Enter the URL of the video you want to download:")
+
+# Download the video when the user clicks the "Download" button
+if st.button("Download"):
+    download_video(url)
