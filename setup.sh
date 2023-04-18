75ba6a97b1aecea7d4356374b0292a5def4d84a6
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-04-18 10:31:35 +0200

    preparing for deployment to heroku part 2

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"tarimicac@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+pip install -r requirements.txt
